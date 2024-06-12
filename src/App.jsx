185801f2b4cdbeb850194e541cbc8b799c5a1958
mohamuhsin <muhsinohoho@outlook.com>
2024-06-12 16:37:49 +0300

    created another input component to avoid repetition and rendered everythoing to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import NewProject from "./components/NewProject";
 import ProjectsSideBar from "./components/ProjectsSideBar";
 
 function App() {
   return (
-    <main className="h-screen my-8">
+    <main className="h-screen my-8 flex gap-8">
       <ProjectsSideBar />
+      <NewProject />
     </main>
   );
 }
