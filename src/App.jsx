22b2ef799640723f6f0a7bb0e755e0aea5632d88
mohamuhsin <muhsinohoho@outlook.com>
2024-07-03 20:33:13 +0300

    we are able to show form data in side bar

--- src/App.jsx
+++ src/App.jsx
@@ -46,7 +46,10 @@ function App() {
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+      <ProjectsSideBar
+        onStartAddProject={handleStartAddProject}
+        projects={projectState.projects}
+      />
       {content}
     </main>
   );
