5c52aaf429340c89dfa26a915aa87aaab04291f1
mohamuhsin <muhsinohoho@outlook.com>
2024-07-04 19:28:18 +0300

    styled our modal and also worked on the cancel button

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,15 @@ function App() {
     });
   }
 
+  function handleCancelAddProject() {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+      };
+    });
+  }
+
   function handleAddProject(projectData) {
     setProjectState((prevState) => {
       const newProject = {
@@ -39,7 +48,9 @@ function App() {
   let content;
 
   if (projectState.selectedProjectId === null) {
-    content = <NewProject onAdd={handleAddProject} />;
+    content = (
+      <NewProject onAdd={handleAddProject} onCancel={handleCancelAddProject} />
+    );
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,6 +2,8 @@ import { forwardRef, useImperativeHandle, useRef } from "react";
 
 import { createPortal } from "react-dom";
 
+import Button from "./Button";
+
 const Modal = forwardRef(function Modal({ children, btnCaption }, ref) {
     const dialog = useRef();
     useImperativeHandle(ref, () => {
@@ -12,10 +14,13 @@ const Modal = forwardRef(function Modal({ children, btnCaption }, ref) {
         };
     });
     return createPortal(
-        <dialog ref={dialog}>
+        <dialog
+            ref={dialog}
+            className="backdrop:bg-stone-900/90 p-4 rounded-sm shadow-md"
+        >
             {children}
-            <form method="dialog">
-                <button>{btnCaption}</button>
+            <form method="dialog" className="mt-4 text-right">
+                <Button>{btnCaption}</Button>
             </form>
         </dialog>,
         document.getElementById("modal-root")
