227a0696fa6ac85de7eb5fe9a0b2603513d0da77
mohamuhsin <muhsinohoho@outlook.com>
2024-07-04 19:15:55 +0300

    added a condition to display modal when invalid inputs are entered into the fields and also used refs and portals including forwardRef and useImperativeHandle to make our modal component reusale

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import Input from "./Input";
+import Modal from "./Modal";
 import { useRef } from "react";
 
 export default function NewProject({ onAdd }) {
+    const modal = useRef();
     const title = useRef();
     const description = useRef();
     const dueDate = useRef();
@@ -11,6 +13,15 @@ export default function NewProject({ onAdd }) {
         const enteredDescription = description.current.value;
         const enteredDueDate = dueDate.current.value;
 
+        if (
+            enteredTitle.trim() === "" ||
+            enteredDescription.trim() === "" ||
+            enteredDueDate.trim() === ""
+        ) {
+            modal.current.open();
+            return;
+        }
+
         onAdd({
             title: enteredTitle,
             description: enteredDescription,
@@ -19,28 +30,35 @@ export default function NewProject({ onAdd }) {
     }
 
     return (
-        <div className="w-[35rem] mt-16">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li>
-                    <button className="text-stone-800 hover:text-stone-950">
-                        Cancel
-                    </button>
-                </li>
-                <li>
-                    <button
-                        className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-900"
-                        onClick={handleSave}
-                    >
-                        Save
-                    </button>
-                </li>
-            </menu>
+        <>
+            <Modal ref={modal} btnCaption="Close">
+                <h2>Invalid input</h2>
+                <p>Ooops... Looks like you forgot to enter a valid value</p>
+                <p>Please make sue you provide a valid value for every input field</p>
+            </Modal>
+            <div className="w-[35rem] mt-16">
+                <menu className="flex items-center justify-end gap-4 my-4">
+                    <li>
+                        <button className="text-stone-800 hover:text-stone-950">
+                            Cancel
+                        </button>
+                    </li>
+                    <li>
+                        <button
+                            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-900"
+                            onClick={handleSave}
+                        >
+                            Save
+                        </button>
+                    </li>
+                </menu>
 
-            <div>
-                <Input type="text" label="Title" ref={title} />
-                <Input label="Description" textarea ref={description} />
-                <Input type="date" label="Due Date" ref={dueDate} />
+                <div>
+                    <Input type="text" label="Title" ref={title} />
+                    <Input label="Description" textarea ref={description} />
+                    <Input type="date" label="Due Date" ref={dueDate} />
+                </div>
             </div>
-        </div>
+        </>
     );
 }
