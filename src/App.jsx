3b63d95bc302d6d3151cbb2366980a25607bf3ac
mohamuhsin <muhsinohoho@outlook.com>
2024-07-06 22:35:15 +0300

    managed task deletion

--- src/App.jsx
+++ src/App.jsx
@@ -28,7 +28,14 @@ function App() {
     });
   }
 
-  function handleDeleteTask() { }
+  function handleDeleteTask(id) {
+    setProjectState((prevState) => {
+      return {
+        ...prevState,
+        tasks: prevState.tasks.filter((task) => task.id !== id),
+      };
+    });
+  }
 
   function handleDeleteProject() {
     setProjectState((prevState) => {
